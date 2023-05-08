29d990a100396e5b4ac18e1c0773ec07d96c58af
gregoryrubies <GregoryRubies@gmail.com>
2023-05-08 21:39:23 +0300

    feat: add 04-copy-directory functionality

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,28 @@
+const { join } = require('node:path');
+const { copyFile, mkdir, rm, readdir } = require('node:fs/promises');
 
+const curfolder = join(__dirname, "files");
+const newFolder = join(__dirname, "files-copy");
+
+/**
+ * 
+ * копирует содержимое папки **files** в папку **files-copy**
+ * 
+ */
+
+async function copyDir() {
+  try {
+    const files = await readdir(curfolder, { withFileTypes: true });
+    await rm(newFolder, { recursive: true, force: true });
+    await mkdir(newFolder, { recursive: true, force: true });
+    files.forEach(file => {
+      if(file.isFile()){
+        copyFile(join(curfolder, file.name), join(newFolder, file.name));
+      }
+    })
+  } catch {
+    console.log("что-то пошло не так");
+  }
+}
+
+copyDir()
